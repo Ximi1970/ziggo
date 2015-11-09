069d383157208e233b2d97e8a4268a320514e84e
Ximi1970 <ximi.github@gmail.com>
2015-11-09 23:08:46 +0100

    Changed cookie

--- alert_ipchange.sh
+++ alert_ipchange.sh
@@ -10,18 +10,18 @@ MODEM_IP=192.168.178.1
 #
 # Login
 #
-curl -L -s -c cookie_checkip.txt -d "Zigloginnaam=ziggo&Zigpassword=draadloos" http://$MODEM_IP/goform/login_zig > /dev/null
+curl -L -s -c cookie_alertchange.txt -d "Zigloginnaam=ziggo&Zigpassword=draadloos" http://$MODEM_IP/goform/login_zig > /dev/null
 
 #
 # Status page, get IP
 #
-CUR_IP=`curl -L -s -c cookie_checkip.txt http://$MODEM_IP/Status.asp | grep "InternetIPAddress" | sed -e "s?.*<b>\(.*\)</b>.*?\1?"`
+CUR_IP=`curl -L -s -c cookie_alertchange.txt http://$MODEM_IP/Status.asp | grep "InternetIPAddress" | sed -e "s?.*<b>\(.*\)</b>.*?\1?"`
 
 #
 # Logout
 #
-curl -L -s -b cookie_checkip.txt -c cookie_checkip.txt --referer http://$MODEM_IP/Status.asp http://$MODEM_IP/logout.asp > /dev/null
-rm -f cookie_checkip.txt
+curl -L -s -b cookie_alertchange.txt -c cookie_alertchange.txt --referer http://$MODEM_IP/Status.asp http://$MODEM_IP/logout.asp > /dev/null
+rm -f cookie_alertchange.txt
 
 #
 # Get the old IP
